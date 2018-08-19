39306a175be4ae2a3db7b8d9648bded71994e462
Wei Ning <nwcgnw@gmail.com>
2018-08-19 18:56:43 +1200

    - update pretty-print example, graph.cpp
    this example helps to replicate a flawed program (details are commented
    in source);
    
    while debug this program I need a customized pretty-print macro to
    inspect the details of the object via layers of pointers

--- inspection/prettyprint/graph.cpp
+++ inspection/prettyprint/graph.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <numeric>
 #include <vector>
+#include <cassert>
+
+namespace core {
 
 using namespace std;
 
@@ -7,32 +12,69 @@ public:
     Parameter(int v)
       : m_value(v) {
     }
+    int get() {
+        return m_value;
+    }
 private:
     int m_value = 0;
 };
 
+// create a shady side effect
+void mutate(Parameter*& param) {
+    int value = param->get();
+
+    // when the parameter's value is 38, nullify this parameter
+    if (value % 30 == 8) {
+        delete(param);
+        param = nullptr;
+    }
+}
+
 class Node {
 public:
     Node(int numParams) {
+        assert(numParams > 0);
         for (int i = 0; i < numParams; ++i) {
             m_params.emplace_back(new Parameter(i));
         }
     }
     ~Node() {
         for (auto param : m_params) {
-            delete(param);
+            if (param) {
+                delete(param);
+            }
         }
+    }
+    void setFirst(int value) {
+        if (m_params[0]) {
+            delete(m_params[0]);
+        }
+        m_params[0] = new Parameter(value);
+        mutate(m_params[0]);
+    }
+    int compute() {
+        return std::accumulate(
+            m_params.begin(),
+            m_params.end(),
+            m_params[0]->get(),
+            [](int init, Parameter* oneParam) -> int {
+                return init + oneParam->get();
+            }
+        );
     }
 private:
     vector<Parameter *> m_params;
 };
 
-namespace core { namespace graph {
+namespace graph {
+
 class Graph {
 public:
-    Graph(int numNodes) {
+    Graph(int numNodes, int argument)
+      : m_argument(argument) {
+        assert(numNodes > 0);
         for (int i = 0; i < numNodes; ++i) {
-            m_nodes.emplace_back(new Node(i));
+            m_nodes.emplace_back(new Node(i + 1));
         }
     }
     ~Graph() {
@@ -40,23 +82,33 @@ public:
             delete(node);
         }
     }
+    int compute() {
+        int arg = m_argument;
+        for (Node* node : m_nodes) {
+            node->setFirst(arg);
+            arg = node->compute();
+        }
+        return arg;
+    }
 private:
     vector<Node *> m_nodes;
+    int m_argument;
 };
 
 } // namespace graph
+
 } // namespace core
 
 void processGraph(core::graph::Graph& graph) {
-    ;
+    graph.compute();
 }
 
 class Graph {
-    vector<void *> m_data;
+    std::vector<void *> m_data;
 };
 
 int main() {
-    core::graph::Graph g(3);
+    core::graph::Graph g(3, 0x13);
     Graph phony;
     processGraph(g);
     return 0;
