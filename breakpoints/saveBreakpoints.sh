290aa0c333b8554fe58e1af5af2ec931e613b455
Wei Ning <wning@wetafx.co.nz>
2018-08-21 08:36:20 +1200

    update save breakpoints example

--- breakpoints/saveBreakpoints.sh
+++ breakpoints/saveBreakpoints.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# update
+# this feature can be a time saver when debugging a complex issue;
+# note: use source command to load the text file back (not load)
+
 function setUp() {
     sutSrc="$( dirname ${0} )/$( basename ${0%.sh} ).cpp"
     sutBin="/tmp/$( basename ${0%.sh} ).o"
